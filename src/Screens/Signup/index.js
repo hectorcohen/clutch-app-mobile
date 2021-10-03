d3cc715015e13f541418962711775dd26d037bec
Hector Cohen <hectorcohenmaldonado@gmail.com>
2021-10-02 19:42:09 -0500

    update

--- src/Screens/Signup/index.js
+++ src/Screens/Signup/index.js
@@ -2,14 +2,14 @@ import React from 'react'
 import {View, Text, StyleSheet, TouchableOpacity} from "react-native";
 import {Color} from "../../theme/Colors";
 
-export default function Signup() {
+export default function Signup({navigation}) {
 	return (
 		<View style={styles.container}>
 			<Text style={styles.title}>Banking never have been so easy</Text>
-				<TouchableOpacity style={styles.buttonSignup}>
+				<TouchableOpacity style={styles.buttonSignup} onPress={() => navigation.navigate('Home')}>
 					<Text style={{fontSize: 18, fontWeight: 'bold'}}>Sign up</Text>
 				</TouchableOpacity>
-			<Text style={{fontWeight: '500', fontSize: 20, color: Color.greyColor, margin: 40}}>Already have an account ? <Text style={{color: Color.whiteColor, fontSize: 20, fontWeight: 'bold'}}>Login</Text></Text>
+			<Text style={{fontWeight: '500', fontSize: 20, color: Color.greyColor, margin: 40}}>Already have an account ? <Text onPress={() => navigation.navigate('Home')} style={{color: Color.whiteColor, fontSize: 20, fontWeight: 'bold'}}>Login</Text></Text>
 		</View>
 	)
 }
